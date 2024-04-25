2fc6f20446dfcde0bc132ad852d0a14affb274b1
BaMau <bamau97@gmail.com>
2024-04-25 16:21:04 +0700

    test(event): add alert event click button (dev)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,14 @@ import { sendGAEvent } from '@next/third-parties/google'
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <button onClick={() => sendGAEvent({ event: 'buttonClicked', value: 'xyz' })}>Send Event</button>
+      <button
+        onClick={() => {
+          alert('button_clicked')
+          sendGAEvent({ event: 'buttonClicked', value: 'click' })
+        }}
+      >
+        Send Event
+      </button>
     </main>
   )
 }
