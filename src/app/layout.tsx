41f232a25d97dfaf17560b082c3265408bb86fde
BaMau <bamau97@gmail.com>
2024-04-25 17:16:09 +0700

    chore: add data layer for ga (dev)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>{children}</body>
-      <GoogleAnalytics gaId='G-CCR7P4N45X' />
+      <GoogleAnalytics gaId='G-CCR7P4N45X' dataLayerName='dataLayer' />
     </html>
   )
 }
